302d0655c5b58f2809a60ce4e75bdfd919bd6310
baisali pradhan <baisali.pradhan@infoviz.co>
2020-08-28 22:21:24 +0530

    Updates

---
+++ precache-manifest.54b44b60f761739f2f083e3b92ddeaba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79167bab844b82d46fc55beedf06b183",
+    "url": "/Recipe-book/index.html"
+  },
+  {
+    "revision": "dee93d94ec8f16304fc1",
+    "url": "/Recipe-book/static/css/main.d34a1f80.chunk.css"
+  },
+  {
+    "revision": "a6d1ebf6515c91a53dde",
+    "url": "/Recipe-book/static/js/2.60e0b9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-book/static/js/2.60e0b9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee93d94ec8f16304fc1",
+    "url": "/Recipe-book/static/js/main.cc16b264.chunk.js"
+  },
+  {
+    "revision": "8e63718253fd3456ea6c",
+    "url": "/Recipe-book/static/js/runtime-main.64103550.js"
+  }
+]);
